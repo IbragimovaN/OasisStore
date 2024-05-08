577bd0fe6b23e597d00b498536036778b9845d94
Anastasiya <anastasiya.ibr@mail.ru>
2024-05-08 12:38:21 +0300

    добавлен обработчик путей

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import routes from "./routes/index.js";
+import path from "path";
 
 const port = 3003;
 const app = express();
@@ -11,6 +12,14 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/", routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("../frontend/build", "index.html"));
+  });
+}
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING).then(() => {
   app.listen(port, async () => {
